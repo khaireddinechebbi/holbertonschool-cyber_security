95b5b78112b5a2fadb71651e49d560e19a24af5c
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2025-02-17 12:37:56 +0100

    openSSL

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo -n "$1" | openssl dgst -sha512 | sed 's/SHA2-512/SHA512/' > 3_hash.txt
+SALT=$(openssl rand -hex 8)
+COMBINED="$1$SALT"
+echo -n "$COMBINED" | openssl dgst -sha512 | sed 's/SHA2-512/SHA512/' > 3_hash.txt
