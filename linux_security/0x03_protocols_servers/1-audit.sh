5537d12ca74f753faa8a8c6c56202cb196498c1d
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2025-02-25 16:43:40 +0100

    audit

---
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+grep -E "Include|KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive" /etc/ssh/sshd_config || echo "Non-standard SSH configuration found."
