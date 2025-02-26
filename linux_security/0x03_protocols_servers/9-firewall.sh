ebd11acaefe144daa824ce063ae416615bdc5dd3
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2025-02-26 11:09:57 +0100

    firewall

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-iptables -F && iptables -P INPUT DROP && iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -i lo -j ACCEPT && iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT && iptables -P OUTPUT ACCEPT
+iptables -F
+iptables -P INPUT DROP && iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -i lo -j ACCEPT && iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT && iptables -P OUTPUT ACCEPT
